b61239716b529f3a630019cdd764129a4aa8da75
Pc <Pc@192.168.150.85>
2022-10-05 11:38:18 +0200

    Interfaccia grafica con bottoni ancora non interagibili

--- src/WaterBalanceFX.java
+++ src/WaterBalanceFX.java
@@ -1,6 +1,15 @@
 
 import java.util.Scanner;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -66,7 +75,83 @@ public class WaterBalanceFX extends Application{
 
     @Override
     public void start(Stage window) throws Exception {
-        window.setTitle("Hello World!");
+        //Creo una schermata con titolo Bilancio Idrico
+        //Inserisco una colonna con titolo "setup" e 4 bottoni per il setup
+        //Inserisco una colonna con titolo "registration" e 3 bottoni per la registrazione
+        //Inserisco una colonna con titolo "report" e 4 bottoni per il report
+        //Inserisco un bottone per il calcolo dell'Ete
+        
+        FlowPane layout = new FlowPane();
+        
+        //Colonna del setup
+        VBox setup = new VBox();
+        setup.setSpacing(5);
+        setup.setPadding(new Insets(10,10,10,10));
+        setup.setAlignment(Pos.CENTER);
+        
+        Button button = new Button("setup station");
+        Button button1 = new Button("setup field");
+        Button button2 = new Button("setup constant");
+        Button button3 = new Button("setup calculation");
+        
+        setup.getChildren().add(new Label("Setup:"));
+        setup.getChildren().add(button);
+        setup.getChildren().add(button1);
+        setup.getChildren().add(button2);
+        setup.getChildren().add(button3);
+        
+        //Colonna del register
+        VBox register = new VBox();
+        register.setSpacing(5);
+        register.setPadding(new Insets(10,10,10,10));
+        register.setAlignment(Pos.CENTER);
+        
+        Button button4 = new Button("register station");
+        Button button5 = new Button("register field");
+        Button button6 = new Button("register constant");
+        //Button button7 = new Button("register calculation");
+        
+        register.getChildren().add(new Label("Register:"));
+        register.getChildren().add(button4);
+        register.getChildren().add(button5);
+        register.getChildren().add(button6);
+        //register.getChildren().add(button7);
+        
+        //Colonna del report
+        VBox report = new VBox();
+        report.setSpacing(5);
+        report.setPadding(new Insets(10,10,10,10));
+        report.setAlignment(Pos.CENTER);
+        
+        Button button8 = new Button("report station");
+        Button button9 = new Button("report field");
+        Button button10 = new Button("report constant");
+        Button button11 = new Button("report calculation");
+        
+        report.getChildren().add(new Label("Report:"));
+        report.getChildren().add(button8);
+        report.getChildren().add(button9);
+        report.getChildren().add(button10);
+        report.getChildren().add(button11);
+        
+        //Colonna dell'Ete
+        VBox ete = new VBox();
+        ete.setSpacing(5);
+        ete.setPadding(new Insets(10,10,10,10));
+        ete.setAlignment(Pos.CENTER);
+        
+        Button button12 = new Button("ete");
+        
+        ete.getChildren().add(new Label("Ete:"));
+        ete.getChildren().add(button12);
+        
+        layout.getChildren().addAll(setup,register,report,ete);
+        
+        Scene scene = new Scene(layout);
+        
+        window.setScene(scene);
+        window.setTitle("Bilancio idrico");
         window.show();
+        
     }
 }
